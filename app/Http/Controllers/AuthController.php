e740a9e1f26b25ed12e7ee32714a0fbd2f61368a
jordan Tomegah <tomegahjordan81@gmail.com>
2024-06-08 03:12:44 +0000

    creation de la login view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 {
 
     public function register() {
-        return view('')
+        return view('auth.register');
     }
     public function Doregister(RegisterRequest $request){
         $client = new Client();
