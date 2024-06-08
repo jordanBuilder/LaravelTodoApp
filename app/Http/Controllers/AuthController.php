e11093665a57f6d6d3c1bc548e5530a56aece10d
jordan Tomegah <tomegahjordan81@gmail.com>
2024-06-08 02:34:09 +0000

    creation de la migration client

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
 
     public function register(RegisterRequest $request){
-        $credentials = $request->validated();
+        $client = new Client();
+       
+
     }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Client extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+     /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
